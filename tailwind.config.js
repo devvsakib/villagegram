4c676f84d6c68fe3a9f240373e6870ad1fc1f1c4
DevvSakib <devvsakib@gmail.com>
2023-07-18 07:16:15 +0600

    starter project

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/**/*.{jsx, js, ts, tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
