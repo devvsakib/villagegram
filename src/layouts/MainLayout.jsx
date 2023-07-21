fd3f6f0a0f65ef22c261c1d445c612103892225f
DevvSakib <devvsakib@gmail.com>
2023-07-21 23:44:00 +0600

    loading screen moved to mainLayout com

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import { useEffect, useState } from "react"
 import { Outlet } from 'react-router-dom'
 import Sidebar from '../components/Sidebar/Sidebar'
+import LoadingScreen from '../components/common/LoadingScreen'
 
 const MainLayout = () => {
+    const [loading, setLoading] = useState(true)
+    useEffect(() => {
+        setTimeout(() => setLoading(false), 2300)
+        document.body.style.overflow = 'hidden'
+        return () => {
+            document.body.style.overflow = 'unset'
+        }
+
+    }, [])
     return (
         <div id='mainLayout' className='flex'>
+            {
+                loading && <div className='fixed top-0 left-0 w-screen h-screen bg-black z-50 flex justify-center items-center'>
+                    <LoadingScreen />
+                </div>
+            }
             <div>
                 <Sidebar />
             </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,12 @@
-import { useEffect, useState } from "react"
 import LoadingScreen from "../components/Common/LoadingScreen"
 import Feed from "../components/Feed"
 import RightSidebar from "../components/Sidebar/RightSidebar"
 
 const Home = () => {
-    const [loading, setLoading] = useState(true)
-    useEffect(() => {
-        // setTimeout(() => setLoading(false), 2300)
-    }, [])
     return (
         <div className="flex min-h-screen gap-10 pl-10">
-            {!loading ? <LoadingScreen /> : <>
-                <Feed />
-                <RightSidebar />
-            </>
-            }
+            <Feed />
+            <RightSidebar />
         </div>
     )
 }
