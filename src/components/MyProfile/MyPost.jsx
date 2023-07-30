25325ff1b7cc3db6a42ecedd93525e66afd34292
DevvSakib <devvsakib@gmail.com>
2023-07-30 23:35:51 +0600

    tagged and post added

--- src/components/MyProfile/MyPost.jsx
+++ src/components/MyProfile/MyPost.jsx
@@ -1,6 +1,19 @@
 const MyPost = () => {
+  const ar = [0, 1, 2, 3]
+  const imgPath = "devvsakib.jpg"
   return (
-    <div>MyPost</div>
+    <div className="grid grid-cols-3 gap-1 mt-1">
+      {
+        ar.map(el =>
+          <div>
+            <div></div>
+            <div className="h-72 overflow-hidden">
+              <img className="object-bottom" src={`assets/${imgPath}`} alt="" srcset="" />
+            </div>
+          </div>
+        )
+      }
+    </div>
   )
 }
 
